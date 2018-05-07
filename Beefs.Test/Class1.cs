0d0858595da4b1c7e678efe2fd6351424cd9bb5d
Corey Knight <corence@gmail.com>
2018-05-07 11:16:27 +1000

    added NUnit

---
+++ Beefs.Test/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Beefs;
+
+namespace Beefs
+{
+    [TestFixture]
+    public class TestSims
+    {
+        [Test]
+        public void testMath()
+        {
+            Assert.That(1 + 2 == 44);
+        }
+
+        [Test]
+        public void testReference()
+        {
+            Assert.That(new ScanContext() != new ScanContext());
+        }
+    }
+}
