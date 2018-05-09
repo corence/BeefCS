e8c45a8d28c19536008435eb9e6cffe8d6e76fbe
Corey Knight <corence@gmail.com>
2018-05-09 20:53:52 +1000

    first attempts at Aero. It's hard! Needs some good work. Hopefully not palming too much work off to the shepherd.

---
+++ Beefs/src/games/Aero.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beefs.src.games
+{
+    // A made-up game kinda like war3
+    class Aero
+    {
+        public readonly Resource log = new Resource("log");
+        public readonly Resource tree = new Resource("tree");
+        public readonly Resource hatchet = new Resource("hatchet");
+        public readonly Resource constructionSite = new Resource("construction site");
+        public readonly Resource delay = new Resource("delay");
+        public readonly Resource x = new Resource("x");
+        public readonly Resource z = new Resource("z");
+
+        public Task ChopTree(double x, double z)
+        {
+            Dictionary<Resource, double> needs = new Dictionary<Resource, double>
+            {
+                { hatchet, 1 },
+                { tree, 1 }
+            };
+            Dictionary<Resource, double> positions = new Dictionary<Resource, double>
+            {
+                { Aero.x, x },
+                { Aero.z, z }
+            };
+            Dictionary<Resource, double> outcomes = new Dictionary<Resource, double>
+            {
+                { log, 1 },
+                { tree, -1 },
+                { delay, 7 }
+            };
+            return new Task("serve dinner", needs, positions, outcomes);
+        }
+
+        public Task FortifyConstruction(long itemId, double x, double z)
+        {
+            Dictionary<Resource, double> needs = new Dictionary<Resource, double>
+            {
+                { log, 1 }
+            };
+            Dictionary<Resource, double> positions = new Dictionary<Resource, double>
+            {
+                { Aero.x, x },
+                { Aero.z, z }
+            };
+            Dictionary<Resource, double> outcomes = new Dictionary<Resource, double>
+            {
+                { log, -1 },
+                { 
+            };
+            return new Task("serve dinner", needs, positions, outcomes);
+        }
+    }
+}
