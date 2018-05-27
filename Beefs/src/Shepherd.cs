b8ef417b17b41a873a4cae9085c044995f257c37
Corey Knight <corence@gmail.com>
2018-05-27 18:56:37 +1000

    some trivial edits, aiming toward creating task optimisers

--- Beefs/src/Shepherd.cs
+++ Beefs/src/Shepherd.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// This is an additional layer that wraps scanning.
+// It is responsible for the following:
+// - when tasks are not being picked, but they are important, associated Rate tasks should gradually increase in desire
+// - when tasks are completed, they should drop in desire
 namespace Beefs
 {
     class Shepherd
@@ -13,6 +18,11 @@ namespace Beefs
         public Shepherd()
         {
             this.scanner = new Scanner();
+        }
+
+        public Task Scan(ScanContext context, IReadOnlyDictionary<Resource, double> initialInventory, IReadOnlyDictionary<Resource, double> initialPositions)
+        {
+            ScanSpot spot = scanner.ScanForSpots(context, initialInventory, initialPositions);
         }
     }
 }
--- Beefs/src/games/Aero.cs
+++ Beefs/src/games/Aero.cs
@@ -11,11 +11,55 @@ namespace Beefs.src.games
     {
         public readonly Resource log = new Resource("log");
         public readonly Resource tree = new Resource("tree");
+        public readonly Resource spear = new Resource("spear");
         public readonly Resource hatchet = new Resource("hatchet");
         public readonly Resource constructionSite = new Resource("construction site");
         public readonly Resource delay = new Resource("delay");
         public readonly Resource x = new Resource("x");
         public readonly Resource z = new Resource("z");
+
+        public Task CraftSpear(double x, double z)
+        {
+            Dictionary<Resource, double> needs = new Dictionary<Resource, double>
+            {
+                { hatchet, 1 },
+                { log, 3 } // ideally they should be dropping logs at the workshop rather than carrying them around. But how :thinking_face: Ahh, separate tasks -- the CreateSpear task wouldn't appear until 3 logs were dropped in
+            };
+            Dictionary<Resource, double> positions = new Dictionary<Resource, double>
+            {
+                { this.x, x },
+                { this.z, z }
+            };
+            Dictionary<Resource, double> outcomes = new Dictionary<Resource, double>
+            {
+                { log, -3 },
+                { spear, 1 },
+                { delay, 4 }
+            };
+            return new Task("craft spear", needs, positions, outcomes);
+        }
+
+	/*
+        public Task ConstructSpearFactory(double x, double z)
+        {
+            Dictionary<Resource, double> needs = new Dictionary<Resource, double>
+            {
+                { log, 3 } // ideally they should be dropping logs at the workshop rather than carrying them around. But how :thinking_face: Ahh, separate tasks -- the CreateSpear task wouldn't appear until 3 logs were dropped in
+            };
+            Dictionary<Resource, double> positions = new Dictionary<Resource, double>
+            {
+                { this.x, x },
+                { this.z, z }
+            };
+            Dictionary<Resource, double> outcomes = new Dictionary<Resource, double>
+            {
+                { log, -3 },
+                { spear.rate, 1 },
+                { delay, 10 }
+            };
+            return new Task("construct spear factory", needs, positions, outcomes);
+        }
+	*/
 
         public Task ChopTree(double x, double z)
         {
